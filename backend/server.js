f55069be788c837378e235ef2337baf9ab3fc5c5
LunaDeLaet <luna.dl@hotmail.com>
2022-08-28 21:39:00 +0200

    33 publish to heroku

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,10 @@
 import express from 'express'
-import data from './data.js'
+import path from 'path'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import seedRouter from './routes/seedRoutes.js'
 import productRouter from './routes/productRoutes.js'
 import userRouter from './routes/userRoutes.js'
-import cors from 'cors'
 import orderRouter from './routes/orderRoutes.js'
 
 //connect to mongodb:
@@ -21,8 +20,6 @@ mongoose
   })
 
 const app = express()
-
-// app.use(cors())
 
 // app.get('/api/products', (req, res) => {
 //   res.send(data.products)
@@ -55,6 +52,12 @@ app.use('/api/seed', seedRouter)
 app.use('/api/products', productRouter)
 app.use('/api/users', userRouter)
 app.use('/api/orders', orderRouter)
+
+const __dirname = path.resolve()
+app.use(express.static(path.join(__dirname, '/frontend/build')))
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+)
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message })
