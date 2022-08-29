b18e4dbeb7456fbc2a057ec376496ef6a4ec7e4b
LunaDeLaet <luna.dl@hotmail.com>
2022-08-29 12:45:00 +0200

    42 upload product image

--- frontend/src/screens/ProductEditScreen.jsx
+++ frontend/src/screens/ProductEditScreen.jsx
@@ -26,6 +26,16 @@ const reducer = (state, action) => {
       return { ...state, loadingUpdate: false }
     case 'UPDATE_FAIL':
       return { ...state, loadingUpdate: false }
+    case 'UPLOAD_REQUEST':
+      return { ...state, loadingUpload: true, errorUpload: '' }
+    case 'UPLOAD_SUCCESS':
+      return {
+        ...state,
+        loadingUpload: false,
+        errorUpload: '',
+      }
+    case 'UPLOAD_FAIL':
+      return { ...state, loadingUpload: false, errorUpload: action.payload }
     default:
       return state
   }
@@ -38,10 +48,11 @@ export default function ProductEditScreen() {
 
   const { state } = useContext(Store)
   const { userInfo } = state
-  const [{ loading, error, loadingUpdate }, dispatch] = useReducer(reducer, {
-    loading: true,
-    error: '',
-  })
+  const [{ loading, error, loadingUpdate, loadingUpload }, dispatch] =
+    useReducer(reducer, {
+      loading: true,
+      error: '',
+    })
 
   const [name, setName] = useState('')
   const [slug, setSlug] = useState('')
@@ -60,10 +71,9 @@ export default function ProductEditScreen() {
         setName(data.name)
         setSlug(data.slug)
         setPrice(data.price)
-        setImage(data.image)
+        setImage(data.imageUrl)
         setCategory(data.category)
         setCountInStock(data.countInStock)
-        setBrand(data.brand)
         setDescription(data.description)
         dispatch({ type: 'FETCH_SUCCESS' })
       } catch (err) {
@@ -89,7 +99,6 @@ export default function ProductEditScreen() {
           price,
           image,
           category,
-          brand,
           countInStock,
           description,
         },
@@ -105,6 +114,28 @@ export default function ProductEditScreen() {
     } catch (err) {
       toast.error(getError(err))
       dispatch({ type: 'UPDATE_FAIL' })
+    }
+  }
+
+  const uploadFileHandler = async (e) => {
+    const file = e.target.files[0]
+    const bodyFormData = new FormData()
+    bodyFormData.append('file', file)
+    try {
+      dispatch({ type: 'UPLOAD_REQUEST' })
+      const { data } = await axios.post('/api/upload', bodyFormData, {
+        headers: {
+          'Content-Type': 'multipart/form-data',
+          authorization: `Bearer ${userInfo.token}`,
+        },
+      })
+      dispatch({ type: 'UPLOAD_SUCCESS' })
+
+      toast.success('Image uploaded successfully')
+      setImage(data.secure_url)
+    } catch (err) {
+      toast.error(getError(err))
+      dispatch({ type: 'UPLOAD_FAIL', payload: getError(err) })
     }
   }
 
@@ -160,6 +191,16 @@ export default function ProductEditScreen() {
               onChange={(e) => setImage(e.target.value)}
               required
             />
+          </Form.Group>
+          <Form.Group
+            className='mb-3'
+            controlId='imageFile'>
+            <Form.Label>Upload File</Form.Label>
+            <Form.Control
+              type='file'
+              onChange={uploadFileHandler}
+            />
+            {loadingUpload && <LoadingBox></LoadingBox>}
           </Form.Group>
           <Form.Group
             className='mb-3'
