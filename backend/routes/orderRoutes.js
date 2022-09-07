efa9678f0c9ed379e6c70a6789adcf5e695fb358
LunaDeLaet <luna.dl@hotmail.com>
2022-09-07 16:59:59 +0200

    update email

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -76,18 +76,14 @@ orderRouter.post(
 
     // send mail with defined transport object
     let info = await transporter.sendMail({
-      from: '"Kevins Catering Webshop" <admin@gmail.com>', // sender address
-      to: 'kevin@hotmail.com', // list of receivers
+      from: '"Kevins Catering Webshop" <de.laet.luna@gmail.com>', // sender address
+      to: 'luna.dl@hotmail.com', // list of receivers
       subject: 'Nieuwe bestelling!', // Subject line
       html: output, // html body
     })
 
     console.log('Message sent: %s', info.messageId)
-    // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-    // Preview only available when sending through an Ethereal account
     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info))
-    // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 
     main().catch(console.error)
   })
